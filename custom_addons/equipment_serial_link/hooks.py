3b2e4779bafe3d91bc3e1f479cb0d31a64e47777
leonmwila <leonmwils47@gmail.com>
2026-08-04 16:15:38 +0200

    refactor: Update post_init_hook to support multiple call signatures

--- custom_addons/equipment_serial_link/hooks.py
+++ custom_addons/equipment_serial_link/hooks.py
@@ -1,7 +1,14 @@
 from odoo import SUPERUSER_ID, api
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env_or_cr, registry=None):
+    # Support both hook call signatures used by different Odoo runtimes:
+    # 1) post_init_hook(env)
+    # 2) post_init_hook(cr, registry)
+    if registry is None and hasattr(env_or_cr, 'cr'):
+        env = env_or_cr
+    else:
+        env = api.Environment(env_or_cr, SUPERUSER_ID, {})
+
     lots = env['stock.lot'].sudo().search([])
     lots._sync_maintenance_equipment()
